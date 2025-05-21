8f2d91fd88a4054f0ebd56a51c3e3b72cd4e460e
NykoEugen <nyko.eugen@gmail.com>
2025-05-21 16:56:05 +0300

    breadcrumb add

--- furniture/urls.py
+++ furniture/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name: str = "furniture"
 
 urlpatterns = [
-    path("", views.furniture_detail, name="furniture"),
     path("<str:furniture_slug>/", views.furniture_detail, name="furniture_detail"),
 ]
